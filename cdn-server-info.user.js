b00e6dd721d79718b2cecbecc990f7e3c60573b2
zhousulong <parabola@vip.qq.com>
2025-07-18 11:49:37 +0800

    chore: Revert to GitHub URL for @require

--- cdn-server-info.user.js
+++ cdn-server-info.user.js
@@ -10,7 +10,7 @@
 // @match        *://*/*
 // @downloadURL  https://raw.githubusercontent.com/zhousulong/cdn-server-info-userscript/main/cdn-server-info.user.js
 // @updateURL    https://raw.githubusercontent.com/zhousulong/cdn-server-info-userscript/main/cdn-server-info.user.js
-// @require      file://C:/Users/ZHIPAI/Documents/GitHub/cdn-server-info-userscript/cdn-headers.js
+// @require      https://raw.githubusercontent.com/shane-zixu/cdn-server-info-userscript/main/cdn-headers.js
 // @grant        GM_addStyle
 // @run-at       document-idle
 // @noframes
