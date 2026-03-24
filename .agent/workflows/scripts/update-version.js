0dd8962b80a6960e73fe62d17eaf442bc60cc02f
zhousulong <parabola@vip.qq.com>
2026-03-24 16:44:39 +0800

    feat: default to collapsed state and enable moving

--- .agent/workflows/scripts/update-version.js
+++ .agent/workflows/scripts/update-version.js
@@ -52,14 +52,14 @@ function incrementVersion(currentVersion, type) {
     const { major, minor, patch } = parseVersion(currentVersion);
 
     switch (type) {
-        case 'major':
-            return `${major + 1}.0.0`;
-        case 'minor':
-            return `${major}.${minor + 1}.0`;
-        case 'patch':
-            return `${major}.${minor}.${patch + 1}`;
-        default:
-            throw new Error(`未知的版本类型: ${type}`);
+    case 'major':
+        return `${major + 1}.0.0`;
+    case 'minor':
+        return `${major}.${minor + 1}.0`;
+    case 'patch':
+        return `${major}.${minor}.${patch + 1}`;
+    default:
+        throw new Error(`未知的版本类型: ${type}`);
     }
 }
 
